9c9a6700a431184a9d88e2960da77e4accf7f3fa
FullyAngozi <ochuks33@yahoo.com>
2024-02-29 10:40:52 -0500

    finished the homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,40 @@
-
+import { useState, useEffect } from "react";
+import CardComponent from "./components/CardComponent";
+import Navbar from "./components/Navbar";
+import SearchComponent from "./components/SearchComponent";
 
 function App() {
+  const [countries, setCountries] = useState([]);
 
+  useEffect(() => {
+    const fetchCountriesData = async () => {
+      try {
+        const response = await fetch(`https://restcountries.com/v3.1/all`);
+        const data = await response.json();
+        setCountries(data);
+        console.log(data)
+      } catch (error) {
+        console.error("Error fetching country details:", error);
+      }
+    };
+
+    fetchCountriesData();
+  }, []); // Empty dependency array means this effect runs once on mount
 
   return (
-    <>
-    <div className=" "><p className=" font-bold text-3xl text-purple-700">I'm creating a country locator with react rest country api</p></div>
-    </>
-  )
+    <main className="bg-very-dark-blue-background min-h-screen">
+      <Navbar />
+      <div className="container">
+        <SearchComponent />{" "}
+        {/* Assuming SearchComponent doesn't need any props for fetching */}
+        <div className="lg:grid lg:grid-cols-3">
+          {countries.map((country, index) => (
+            <CardComponent key={index} country={country} />
+          ))}
+        </div>
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,31 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    './node_modules/flowbite-react/lib/esm/**/*.js',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark-blue-elements': 'hsl(209,  23%,  22%)',
+        'very-dark-blue-background': 'hsl(207,  26%,  17%)',
+        'very-dark-blue-text': 'hsl(200,  15%,  8%)',
+        'light-gray-input': 'hsl(0,  0%,  52%)',
+        'very-light-gray-background': 'hsl(0,  0%,  98%)',
+        'white': 'hsl(0,  0%,  100%)',
+      },
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem'
+      }
+    }
   },
-  plugins: [],
-}
+  plugins: [
+    require('flowbite/plugin')
+  ],
+};
